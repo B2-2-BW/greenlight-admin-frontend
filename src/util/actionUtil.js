baa8799a2c31d7f28f514b3b0d1880661238f6e5
Daniel Choi <danielchoi1115@gmail.com>
2025-11-11 16:16:03 +0900

    랜딩 URL 변경

--- src/util/actionUtil.js
+++ src/util/actionUtil.js
@@ -2,7 +2,7 @@ const getActionUrl = (actionType, action) => {
   if (actionType == null) {
     return '';
   } else if (actionType === 'LANDING') {
-    return `https://wt.greenlight.winten.im/l/${action?.landingId}`;
+    return `https://greenlight.hyundai-ite.com/l/${action?.landingId}`;
   } else {
     return action?.actionUrl;
   }
